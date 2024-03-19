d3b5283ec1272e13ba2cbfd9b5320177b913a7df
narustudio <slackjawed12@gmail.com>
2024-03-19 22:21:28 +0900

    feature: home page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Post, allPosts } from "contentlayer/generated";
+import { compareDesc } from "date-fns";
 import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main>
-      <div className="flex justify-center align-middle">
+      <div className="flex justify-center align-middle p-10">
         <GlobalNavigation />
         <RecentPosts />
       </div>
@@ -20,22 +21,23 @@ function RecentPosts() {
   return (
     <div className="flex-1">
       <text className="text-center">최근 게시물</text>
-      {allPosts.map((post, i) => {
-        return (
-          <Link key={i} href={post.url}>
-            <PostItem post={post} />
-          </Link>
-        );
-      })}
+      {allPosts
+        .toSorted((o1, o2) => compareDesc(o1.createdAt, o2.createdAt))
+        .map((post, i) => {
+          return <PostItem key={i} post={post} />;
+        })}
     </div>
   );
 }
 
 function PostItem({ post }: { post: Post }) {
   return (
-    <>
-      <h2 className="font-bold py-2 text-xl">{post.title}</h2>
+    <Link href={post.url} className="bg-blue-500">
+      <div className="flex items-center justify-between">
+        <h2 className="font-bold py-2 text-xl">{post.title}</h2>
+        <text>{post.createdAt.split("T")[0]}</text>
+      </div>
       <text>{post.description}</text>
-    </>
+    </Link>
   );
 }
