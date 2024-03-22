b9a196f84181bff44803ec98e8a30dabc9549a35
narustudio <slackjawed12@gmail.com>
2024-03-23 00:28:48 +0900

    feature: import katex css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../styles/globals.css";
 import { Providers } from "./providers";
 import { Header } from "../components/Header";
+
+import "../styles/globals.css";
+import "katex/dist/katex.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
