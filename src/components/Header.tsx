e1e41ae2968852831a3452503ef5ead869eacdb5
narustudio <slackjawed12@gmail.com>
2024-03-25 22:41:59 +0900

    fix: modify header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="p-10 flex justify-between">
+    <header className="py-5 flex justify-between">
       <Link href={"/"}>
         <h1>개발자 이민재입니다.</h1>
       </Link>
