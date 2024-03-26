292878211cbd6bf24f013f453233ac800c058781
narustudio <slackjawed12@gmail.com>
2024-03-26 23:07:47 +0900

    chore: fix category component

---
+++ src/components/GlobalSideNavigation.tsx
@@ -0,0 +1,8 @@
+export function GlobalSideNavigation() {
+  return (
+    <div className="hidden md:flex md:flex-col">
+      <text className="">카테고리</text>
+      <div className="flex"></div>
+    </div>
+  );
+}
