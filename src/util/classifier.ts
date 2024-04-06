2a8198ff8d3350f279eb1e2bb79d5aace4627fa2
narustudio <slackjawed12@gmail.com>
2024-04-06 21:53:21 +0900

    feature: write classify method

--- src/util/classifier.ts
+++ src/util/classifier.ts
@@ -1,3 +1,35 @@
 import { Post } from "contentlayer/generated";
 
-const classifyPostsByDate = (posts: Post[]) => {};
+type PostCategoryInfo = {
+  mainCategory: string;
+  branchCategory: string;
+  posts: Post[];
+};
+
+export const classifyByCategory = (posts: Post[]) => {
+  const store = posts.reduce((prev, cur) => {
+    const tokens = cur.slug.split("/");
+    const categoryByPath = tokens.slice(0, tokens.length - 1);
+    console.log(categoryByPath);
+    if (categoryByPath.length < 1) {
+      const unclassifiedPosts = prev.get("unclassified");
+
+      prev.set(
+        "unclassified",
+        unclassifiedPosts ? [...unclassifiedPosts, cur] : [cur]
+      );
+
+      return prev;
+    }
+    const key = categoryByPath.join("/");
+    const before = prev.get(key);
+    prev.set(categoryByPath.join("/"), before ? [...before, cur] : [cur]);
+    return prev;
+  }, new Map());
+
+  return Array.from(store);
+};
+
+type PostDateInfo = {};
+
+const classifyByDate = () => {};
