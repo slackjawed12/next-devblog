23570f08c2d39e3c7b35a71c70a99ed8fde1a275
narustudio <slackjawed12@gmail.com>
2024-05-12 13:09:24 +0900

    feature: markup reading time

--- src/app/posts/[...slug]/page.tsx
+++ src/app/posts/[...slug]/page.tsx
@@ -1,33 +1,32 @@
-import { format, parseISO } from "date-fns";
-import { allPosts } from "contentlayer/generated";
-import { getMDXComponent } from "next-contentlayer/hooks";
-import Giscus from "src/components/Giscus";
+import { format, parseISO } from 'date-fns';
+import { allPosts } from 'contentlayer/generated';
+import { getMDXComponent } from 'next-contentlayer/hooks';
+import Giscus from 'src/components/Giscus';
 
 export const generateStaticParams = async () => {
-  return allPosts.map((post) => ({ slug: post.slug.split("/") }));
+  return allPosts.map((post) => ({ slug: post.slug.split('/') }));
 };
 
-export const generateMetadata = ({
-  params,
-}: {
-  params: { slug: string[] };
-}) => {
-  const post = allPosts.find((post) => post.slug === params.slug.join("/"));
+export const generateMetadata = ({ params }: { params: { slug: string[] } }) => {
+  const post = allPosts.find((post) => post.slug === params.slug.join('/'));
   return { title: post?.title };
 };
 
 export default function PostLayout({ params }: { params: { slug: string[] } }) {
-  const slugByParams = `${params.slug.join("/")}`;
+  const slugByParams = `${params.slug.join('/')}`;
   const post = allPosts.find((post) => `${post.slug}` === slugByParams);
-  const Content = getMDXComponent(post?.body?.code ?? "");
+  const Content = getMDXComponent(post?.body?.code ?? '');
 
   return (
-    <article className="pt-8 mx-auto max-w-3xl prose prose-sm dark:prose-invert sm:prose-base lg:prose-lg">
-      <div className="mb-8 text-center">
-        <time dateTime={post?.createdAt}>
-          {format(parseISO(post?.createdAt ?? "2024-03-13"), "LLLL d, yyyy")}
-        </time>
-        <h1>{post?.title}</h1>
+    <article className="prose prose-sm mx-auto max-w-3xl pt-8 dark:prose-invert sm:prose-base lg:prose-lg">
+      <div className="mb-1">
+        <h1 className="text-center">{post?.title}</h1>
+        <div className="align-center flex justify-center gap-5">
+          <time dateTime={post?.createdAt} className="my-auto">
+            {format(parseISO(post?.createdAt ?? '0000-00-00'), 'LLLL d, yyyy')}
+          </time>
+          <p className="">{post?.readingTime}min read</p>
+        </div>
       </div>
       <Content />
       <Giscus />
