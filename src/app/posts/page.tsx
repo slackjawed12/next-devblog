fa5927d655cba7607cd9b169426c5c2f18344e0c
narustudio <slackjawed12@gmail.com>
2024-09-29 22:10:47 +0900

    feat: post category feature

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,57 @@
-export default function PostPage() {
+import { allPosts } from 'contentlayer/generated';
+import { compareDesc } from 'date-fns';
+import GlobalSideNavigation from 'src/components/GlobalSideNavigation';
+import PostItem from 'src/components/PostItem';
+import { classifyByCategory } from 'src/util/classifier';
+
+type PostPageProps = {
+  searchParams: {
+    category?: string;
+  };
+};
+
+export default function PostPage({ searchParams }: PostPageProps) {
   return (
     <>
-      <span>게시물 화면 준비중입니다.</span>
+      <main className="min-h-screen">
+        <div className="flex">
+          <GlobalSideNavigation />
+          <CategoryPosts category={searchParams.category} />
+        </div>
+      </main>
     </>
+  );
+}
+
+function CategoryPosts({ category }: { category?: string }) {
+  const copiedPost = allPosts.slice();
+  if (category) {
+    const classified = classifyByCategory(copiedPost).find((v) => v.category === category);
+
+    if (classified) {
+      const targetPosts = classified.posts;
+
+      return (
+        <div className="flex-1">
+          <div className="text-center">{category} 관련 게시물</div>
+          {targetPosts
+            .sort((o1, o2) => compareDesc(o1.createdAt, o2.createdAt))
+            .map((post, i) => {
+              return <PostItem key={i} post={post} />;
+            })}
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div className="flex-1">
+      <div className="text-center">최근 게시물</div>
+      {copiedPost
+        .sort((o1, o2) => compareDesc(o1.createdAt, o2.createdAt))
+        .map((post, i) => {
+          return <PostItem key={i} post={post} />;
+        })}
+    </div>
   );
 }
--- src/components/GlobalSideNavigation.tsx
+++ src/components/GlobalSideNavigation.tsx
@@ -1,27 +1,31 @@
-"use client";
+'use client';
 
-import { allPosts } from "contentlayer/generated";
-import { classifyByCategory } from "src/util/classifier";
+import { allPosts, Post } from 'contentlayer/generated';
+import Link from 'next/link';
+import { classifyByCategory } from 'src/util/classifier';
 
 export default function GlobalSideNavigation() {
   const classifiedPosts = classifyByCategory(allPosts);
-  classifiedPosts.sort();
+  classifiedPosts.sort((o1, o2) => (o1.category < o2.category ? -1 : 1));
+
   return (
-    <div className="hidden md:flex flex-col mr-6">
+    <div className="mr-6 hidden flex-col md:flex">
       <span className="mb-8 text-center">주제별 카테고리</span>
       <CategoryList classified={classifiedPosts} />
     </div>
   );
 }
 
-function CategoryList({ classified }: { classified: any[] }) {
+function CategoryList({ classified }: { classified: { category: string; posts: Post[] }[] }) {
   return (
     <>
       {classified.map((post, index) => {
         return (
-          <div key={index}>
-            <span>{post[0].toUpperCase()}</span>
-          </div>
+          <Link key={index} href={`/posts?category=${post.category}`}>
+            <div className="pb-2 pt-2">
+              <span>{post.category.toUpperCase()}</span>
+            </div>
+          </Link>
         );
       })}
       <div className="flex"></div>
