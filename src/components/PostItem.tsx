25af17bb2f271780230f3785cc6f3320d4b646b6
narustudio <slackjawed12@gmail.com>
2024-09-29 22:10:05 +0900

    feat: extract post item component

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,16 @@
+import { Post } from 'contentlayer/generated';
+import Link from 'next/link';
+
+export default function PostItem({ post }: { post: Post }) {
+  return (
+    <Link href={`/posts/${post.slug}`}>
+      <div className="my-10 flex flex-col">
+        <h2 className="truncate py-2 text-sm font-bold md:text-xl">{post.title}</h2>
+        <span className="py-1 text-sm md:text-lg">{post.description}</span>
+        <span className="py-1 text-xs text-slate-400 md:text-sm">
+          {post.createdAt.split('T')[0]}
+        </span>
+      </div>
+    </Link>
+  );
+}
