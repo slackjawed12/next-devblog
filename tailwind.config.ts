61dc531d280da781dc3b95689e2be8f9d9e54f1f
narustudio <slackjawed12@gmail.com>
2024-11-17 23:11:23 +0900

    feat: theme switcher

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import plugin from 'tailwindcss/plugin';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {},
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require('@tailwindcss/typography'),
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.text-primary-color': {
+          '@apply text-neutral-900 dark:text-neutral-50': '',
+        },
+        '.bg-primary-color': {
+          '@apply bg-neutral-50 dark:bg-neutral-900': '',
+        },
+      });
+    }),
+  ],
 };
 
 export default config;
