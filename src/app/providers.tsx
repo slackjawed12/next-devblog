44edefe5e296014b6651415dddc8ffed6962a9b8
minjaelee <slackjawed12@gmail.com>
2025-01-02 00:57:48 +0900

    feat: 2024 retro

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,19 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [isMount, setMount] = useState(false);
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  if (!isMount) {
+    return null;
+  }
+
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
